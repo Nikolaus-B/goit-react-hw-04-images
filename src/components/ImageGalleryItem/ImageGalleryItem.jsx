af74456ed4a88d87a59e364200f98136db08576a
Mykola <53128883+Nikolaus-B@users.noreply.github.com>
2023-11-15 17:49:00 +0200

    modal + hooks

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,42 +1,27 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { GalleryImage, GalleryItem } from './ImageGalleryItem.styled';
 import { ModalImage } from 'components/ModalImage/ModalImage';
 
-export class ImageGalleryItem extends Component {
-  state = {
-    isModalOpen: false,
+export const ImageGalleryItem = ({ img, largeImg, tag }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
   };
 
-  openModal = () => {
-    this.setState({ isModalOpen: true });
+  const closeModal = () => {
+    setIsModalOpen(false);
   };
 
-  closeModal = () => {
-    this.setState({ isModalOpen: false });
-  };
-
-  render() {
-    const { isModalOpen } = this.state;
-    const { img, largeImg, tag } = this.props;
-
-    return (
-      <GalleryItem>
-        <GalleryImage onClick={this.openModal} src={img} alt={tag} />
-        <ModalImage
-          modalIsOpen={isModalOpen}
-          closeModal={this.closeModal}
-          largeImg={largeImg}
-          topic={tag}
-        />
-      </GalleryItem>
-    );
-  }
-}
-
-// export const ImageGalleryItem = ({ img, largeImg, tags }) => {
-//   return (
-// <GalleryItem>
-//   <GalleryImage src={img} alt={tags} />
-// </GalleryItem>
-//   );
-// };
+  return (
+    <GalleryItem>
+      <GalleryImage onClick={openModal} src={img} alt={tag} />
+      <ModalImage
+        modalIsOpen={isModalOpen}
+        closeModal={closeModal}
+        largeImg={largeImg}
+        topic={tag}
+      />
+    </GalleryItem>
+  );
+};
